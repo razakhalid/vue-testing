6366942f88de74a01ec888e05201b77b46477426
razakhalid <ra97za@gmail.com>
2022-11-16 11:24:19 -0600

    moving to git

---
+++ tests/unit/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <a href="/profile" id="profile">My Profile</a>
+    <a v-if="admin" id="admin" href="/admin" class="profile">Admin</a>
+  </nav>
+<!--  <button @click="increment"></button>-->
+<!--  <div v-if="count % 2 === 0">Count: {{ count }}. Count is even</div>-->
+<!--  <div v-if="count % 2 !== 0">Count: {{ count }}. Count is odd</div>-->
+  <button @click="handleClick"></button>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "App",
+  data() {
+    return {
+      count: 0,
+      admin: false
+    }
+  },
+  computed: {
+    // count() {
+    //   return this.$store.state.count;
+    //   // return this.$store.state.count;
+    // }
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+    },
+    handleClick() {
+      this.count += 1;
+      this.$emit('count', this.count, 'hello');
+    }
+  },
+  // mounted() {
+  //   axios.get('/')
+  // }
+}
+</script>
+
+<style scoped>
+
+</style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,97 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils';
+import { nextTick, reactive } from "vue";
+import App from './App';
+import { createStore } from 'vuex';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe('App', () => {
+  it('renders a profile link', () => {
+    const wrapper = mount(App)
+    expect(wrapper.find('#profile').text()).toBe('My Profile');
+  });
+  it('renders an admin link', () => {
+    const wrapper = mount(App, {
+      data() {
+        return {
+          admin : true
+        }
+      }
+    });
+    expect(wrapper.find('#admin').exists()).toBe(true);
+  });
+  it('does not render an admin link', () => {
+    const wrapper = mount(App, {
+      data() {
+        return {
+          admin : false
+        }
+      }
+    });
+    // expect(wrapper.find('#admin').exists()).not.toBe(true);
+    expect(wrapper.find('#admin').isVisible()).not.toBe(true);
+  });
+});
+
+// test('emits a count with correct payload', async () => {
+//   const wrapper = mount (App);
+//   wrapper.find('button').trigger('click');
+//   console.log(wrapper.emitted().count[0][0]);
+//   expect(wrapper.emitted().count[0][0]).toBe(1);
+//   wrapper.find('button').trigger('click');
+//   expect(wrapper.emitted().count[1][0]).toBe(2);
+//   // console.log(wrapper.emitted());
+// });
+
+/*
+const createVuexStore = () => {
+  return  createStore({
+    state() {
+      return {
+        count: 0
+      }
+    },
+    mutations: {
+      increment(state) {
+        state.count += 1;
+      }
+    }
+});
+}
+
+
+function factory() {
+  const store = createVuexStore();
+  return mount(App, {
+    global: {
+      plugins: [store],
+      mocks: {
+        $route: {
+          params: {
+            postId: 1
+          }
+        }
+      },
+      stubs: {
+      }
+    }
+  });
+}
+
+let mockGet = jest.fn()
+jest.mock('axios', () => ({
+  get: () => mockGet()
+}))
+
+describe('App', () => {
+  it('render count when even', async () => {
+    const wrapper = factory();
+    await wrapper.find('button').trigger('click');
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.html()).toContain('Count: 2. Count is even');
+  });
+  it('render count when odd', async () => {
+    const wrapper = factory();
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.html()).toContain('Count: 1. Count is odd');
+  });
+});
+*/
